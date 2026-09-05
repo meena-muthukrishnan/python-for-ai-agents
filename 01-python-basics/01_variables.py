9848b7fbbc1aa1e55a8969d349da2f0be026fcab
meena-muthukrishnan <meenamuthukrishnanr@gmail.com>
2026-09-06 00:33:22 +0800

    Add variables and print statements in 01_variables.py
    Added variables for name, role, experience, and learning AI. Included print statements for output.

--- 01-python-basics/01_variables.py
+++ 01-python-basics/01_variables.py
@@ -1,1 +1,12 @@
+name = "Meena"
+role = "Senior RPA developer"
+experience = 7
+learning_ai =True
 
+print(name)
+print(role)
+print(experience)
+print(learning_ai)
+
+
+print(f"My name is {name} and am a {role} with {experience} of experience. Currently, I am learing ai agents {learning_ai}")
